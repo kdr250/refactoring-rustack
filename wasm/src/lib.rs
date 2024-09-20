0c3f5273211e69e523a09a729d91171e4160bc7f
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-09-20 16:40:07 +0900

    とりあえず画面表示時にコンソールに実行結果を出力してみる

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,13 +1,32 @@
 mod utils;
 
+use refactoring_rustack::{Parser, VirtualMachine};
 use wasm_bindgen::prelude::*;
+use web_sys::console::log_1;
 
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
 }
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm!");
+#[wasm_bindgen(start)]
+pub fn evaluate() {
+    let mut virtual_machine = VirtualMachine::new();
+    let mut parser = Parser::new();
+
+    let lines = r#"
+/x 10 def
+/y 20 def
+{ x y < } { x } { y } if
+"#;
+
+    for line in lines.lines() {
+        for element in parser.parse(line.to_string()) {
+            virtual_machine.evaluate(element);
+        }
+    }
+
+    let log_str = format!("{:?}", virtual_machine.stack().list());
+
+    log_1(&JsValue::from_str(log_str.as_str())); // [Number(10)]
 }
