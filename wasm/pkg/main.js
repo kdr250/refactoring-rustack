559fe75195e240ec7d70cf2c28a650af70fc3708
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-09-23 15:09:19 +0900

    ブラウザ上でサンプルファイルを選択できるようにする

--- wasm/pkg/main.js
+++ wasm/pkg/main.js
@@ -5,8 +5,30 @@ async function initialize() {
 
     set_panic_hook();
 
+    generateSamples();
+
     document.getElementById("run").addEventListener("click", () => run());
     document.getElementById("run-image").addEventListener("click", () => runImage());
+}
+
+function generateSamples() {
+    const samples = document.getElementById("samples");
+
+    ["while_gray.txt", "mandel.txt"]
+        .forEach(fileName => {
+            const link = document.createElement("a");
+            link.href = "#";
+            link.addEventListener("click", () => {
+                fetch("scripts/" + fileName)
+                    .then(file => file.text())
+                    .then(text => {
+                        document.getElementById("input").value = text
+                    });
+            });
+            link.innerHTML = fileName;
+            samples.appendChild(link);
+            samples.append(" ");
+        })
 }
 
 function run() {
